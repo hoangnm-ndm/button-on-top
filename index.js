24729e24c1aec7d4291686bd229d8fb04d065205
hoangnm <hoangnm.ndm@gmail.com>
2023-03-04 17:11:57 +0700

    create mini project btn-on-top

---
+++ index.js
@@ -0,0 +1,19 @@
+// Get the button
+let btnOnTop = document.getElementById("btnOnTop");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    btnOnTop.style.display = "block";
+  } else {
+    btnOnTop.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+}
